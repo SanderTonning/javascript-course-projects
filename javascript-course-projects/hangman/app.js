50012b88a5beac14e19cdf08b9c36a8e13bf2feb
Sander Tonning <sandertonning@Sanders-MacBook-Pro.local>
2020-04-22 15:17:18 +0200

    change to use fetch

--- javascript-course-projects/hangman/app.js
+++ javascript-course-projects/hangman/app.js
@@ -18,12 +18,24 @@ window.addEventListener('keypress', (e) => {
 
 getPuzzle('2').then((puzzle) => {
     console.log(puzzle)
-}, (error) => {
+}).catch((error) => {
     console.log(`Error: ${error}`)
 })
 
 getCountry('NO').then((country) => {
     console.log(country.name)
-}, (error) => {
+}).catch((error) => {
     console.log(`Error: ${error}`)
 })
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if(response.status === 200) {
+//         return response.json()
+//     } else {
+//         throw new Error('Unable to fetch the puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
